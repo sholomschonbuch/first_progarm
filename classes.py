71fc9675b033f550488299b450911ed75a566f72
Sholom <sholomschonbuch@gmail.com>
2020-06-08 20:08:03 -0400

    second commit

--- classes.py
+++ classes.py
@@ -1,32 +1,41 @@
+class job:
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+        self.categorie_list = []
+        
+
+
+
+
 class categorie:
     def __init__(self, name):
         self.name = name
-        self.initial_total = []
         self.amount = []
         self.date = []
         self.paid = []
         self.paid_date = []
         
     def add(self, amount, date):
-        #user_input = float(input("How much do you owe? "))
-        #input_date = input("What is the date? ")
         self.amount.append(amount)
         self.date.append(date)
+
     def add_paid(self, amount, date):
-        #user_input = float(input("How much did you pay? "))
-        #input_date = input("What is the date? ")
         self.paid.append(amount)
         self.paid_date.append(date)
+
     def total(self):
         total_amount = 0
         for money in self.amount:
             total_amount += money
         return total_amount
+
     def total_paid(self):
         total_amount = 0
         for money in self.paid:
             total_amount += money
         return total_amount
+    def
     def __str__(self):
         return f"Name: {self.name} \ntotal: {self.total()} \nowe: {self.total() - self.total_paid()}"
     
